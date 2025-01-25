da959f4851f8bad735e39786fc517a817d4af4ca
Marco Aurélio Mota Vilar <m.amotavilar@gmail.com>
2025-01-25 10:37:10 -0300

    Delete 02 - consulta_age.sql

--- 02 - consulta_age.sql
+++
@@ -1,7 +0,0 @@
--- Seleciona da coluna age e já realiza a média através de avg e renomeia para idade_media
-select
-
-avg(age) as idade_media
-
--- Tabela onde está localizada no banco de dados.
-from leads_basic_details
